35015d8ddd5d688a7fb4615f6b8e8989358c0bb5
aushak anuz <aushakanuz@aushaks-MacBook-Air.local>
2020-08-30 23:36:14 +0530

    nothing

---
+++ logical-operators/logical-and.sql
@@ -0,0 +1,37 @@
+SELECT title, author_lname, released_year
+FROM books
+WHERE author_lname='Eggers';
+
+SELECT title, author_lname, released_year
+FROM books
+WHERE released_year > 2010;
+
+SELECT
+    title,
+    author_lname,
+    released_year
+FROM books
+WHERE author_lname='Eggers'
+    AND released_year > 2010;
+
+SELECT 1
+< 5 AND 7 = 9;
+-- false
+
+SELECT -10
+> -20 AND 0 <= 0;
+-- true
+
+SELECT -40
+<= 0 AND 10 > 40;
+--false
+
+SELECT 54
+<= 54 AND 'a' = 'A';
+-- true
+
+SELECT *
+FROM books
+WHERE author_lname='Eggers'
+    AND released_year > 2010
+    AND title LIKE '%novel%';
